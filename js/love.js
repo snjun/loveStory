fac97cb6f588345b6aad8a39b5078ec787c90af9
Snjun·Q <m1356997@gmail.com>
2018-09-29 03:09:36 +0800

    Update love.js

--- js/love.js
+++ js/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
-            ctx.fillText("媛媛打开", 23, 16);
+            ctx.fillText("媛媛,戳我", 23, 16);
             ctx.restore();
         },
         clear: function () {
